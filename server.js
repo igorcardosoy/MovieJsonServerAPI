07ebe7a5d4041c362c4b64ad2d690ae580d6ad62
igorcardosoy <igorfilipicardoso@gmail.com>
2024-07-02 15:04:00 -0300

    Change servers config

--- server.js
+++ server.js
@@ -5,21 +5,15 @@ const cors = require('cors');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults({noCors: true});
-const port = 5000
+const port = 3500
 
 server.db = router.db
 
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://movies-next-b6tkmcj75-igorcardosoys-projects.vercel.app')
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
   res.header('Access-Control-Allow-Headers', '*')
   next()
 })
-
-server.use(
-  jsonServer.rewriter({
-    '/*': '/$1',
-  })
-)
 
 server.use(auth)
 server.use(cors())
@@ -28,5 +22,3 @@ server.use(router)
 server.listen(port, () => {
     console.log(`JSON Server is running in ${port}`);
 });
-
-module.exports = server;
